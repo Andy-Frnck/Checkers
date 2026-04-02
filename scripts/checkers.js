d000d51b67b71c48132c2cb3c8c651d4030b06da
AF <andyfranck211@gmail.com>
2026-04-02 12:37:58 +0200

    creating the board with javascript

--- scripts/checkers.js
+++ scripts/checkers.js
@@ -1,13 +1,33 @@
-let squares = document.querySelectorAll(".square")
+let board = document.querySelector(".board");
 
-let themes = [['#777a92','#161722'],['#f7d564','#4d2a00']]
-let theme = themes[1]
-let color=theme[0];
+let square = ``;
+
+for (let i = 0; i < 64; i++) {
+  square += `
+  <div class="square" id="${i}"></div>`;
+}
+
+board.innerHTML = square;
+
+let squares = document.querySelectorAll(".square");
+
+let themes = [
+  ["#777a92", "#161722"],
+  ["#f7d564", "#4d2a00"],
+];
+let theme = themes[1];
+let color = theme[0];
+
 squares.forEach((square, i) => {
   square.style.backgroundColor = color;
-  if (color === theme[0] && (i+1)%8!==0) {
-    color = theme[1]    
-  }else if(color === theme[1] && (i+1)%8!==0){
-    color = theme[0]
+  if (color === theme[0] && (i + 1) % 8 !== 0) {
+    color = theme[1];
+  } else if (color === theme[1] && (i + 1) % 8 !== 0) {
+    color = theme[0];
+  }
+  if(square.style.backgroundColor === "rgb(77, 42, 0)"){
+    if (i<24 || i>=40) {
+      square.innerHTML =`<div class='piece' ${i>=40 ? `style="background-color: #fff"`:''}></div>`
+    }
   }
 });
